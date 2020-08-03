d6660ed7ec963f81f59dbfd9328e773ea41d5aca
ApostolaT <allex.post26@gmail.com>
2020-08-03 22:40:22 +0300

    setup the application with one custom controller and some custom endpoints

---
+++ src/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/custom/{userName?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request)
+    {
+        $userName = $request->get("userName");
+        return $this->render('home/custom.html.twig', [
+            'name' => $userName
+        ]);
+    }
+}
