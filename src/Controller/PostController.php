9a8f1219743a84d240a7ff9a7bc746b7a9b55aaf
ApostolaT <allex.post26@gmail.com>
2020-08-10 23:27:31 +0300

    implement several CRUD operations on Posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +17,25 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class PostController extends AbstractController
 {
+    private $postRepository;
+    /**
+     * PostController constructor.
+     * @param PostRepository $postRepository
+     */
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+
     /**
      * @Route("/", name="index")
      * @param PostRepository $postRepository
      * @return Response
      */
-    public function index(PostRepository  $postRepository)
+    public function index()
     {
-        $posts = $postRepository->findAll();
+        $posts = $this->postRepository->findAll();
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
@@ -45,6 +57,34 @@ class PostController extends AbstractController
         $em->persist($post);
         $em->flush();
 
-        return new Response("Post was created");
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     * @Route("/show/{id?}", name="show")
+     */
+    public function show(int $id) {
+        $post = $this->postRepository->find($id);
+
+        return $this->render("post/show.html.twig", [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function remove(int $id) {
+        $post = $this->postRepository->find($id);
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl("post.index"));
     }
 }
